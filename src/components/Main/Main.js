9c05dd41e1263d23089d386dae1993ac65a8fd60
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-24 12:32:55 -0800

    added redirect so no 404 on home

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch, Link } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, Link, Redirect } from 'react-router-dom';
 import Colors from '../Colors/Colors.js';
 import NotFound from '../NotFound.js';
 import './Main.css';
@@ -8,16 +8,21 @@ export default function Main() {
   return (
     <BrowserRouter>
       <div className='container'>
-        <Link to='/255/0/0'>
-          <h1>RED</h1>
-        </Link>
-        <Link to='/0/255/0'>
-          <h1>GREEN</h1>
-        </Link>
-        <Link to='/0/0/255'>
-          <h1>BLUE</h1>
-        </Link>
+        <div className='link-container'>
+          <Link to='/255/0/0'>
+            <h1>RED</h1>
+          </Link>
+          <Link to='/0/255/0'>
+            <h1>GREEN</h1>
+          </Link>
+          <Link to='/0/0/255'>
+            <h1>BLUE</h1>
+          </Link>
+        </div>
         <Switch>
+          <Route exact path="/" >
+            <Redirect to="/255/0/0" />
+          </Route>
           <Route path='/:r/:g/:b' component={Colors} />
           <Route path="*" component={NotFound} />
         </Switch>
